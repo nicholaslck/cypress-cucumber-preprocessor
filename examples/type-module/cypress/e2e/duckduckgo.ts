834b65b1d0dfc383b8e6e8b71529f99bb39943f2
Jonas Amundsen <jonasba@gmail.com>
2023-09-13 17:09:12 +0200

    Add an example with type: module
    This is kind of an internal example and currently exists merely to
    ensure that this setup never breaks.
    
    Related to #1093 [1].
    
    [1] https://github.com/badeball/cypress-cucumber-preprocessor/issues/1093

---
+++ examples/type-module/cypress/e2e/duckduckgo.ts
@@ -0,0 +1,15 @@
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When("I visit duckduckgo.com", () => {
+  cy.visit("https://duckduckgo.com/");
+});
+
+Then("I should see a search bar", () => {
+  cy.get("input[type=text]").should(
+    "have.attr",
+    "placeholder",
+    "Search the web without being tracked"
+  );
+
+  assert.deepEqual({}, {});
+});
