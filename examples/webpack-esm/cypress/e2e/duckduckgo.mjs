8a5dd94c8725257f50310011afa0b4a768547403
Jonas Amundsen <jonasba@gmail.com>
2023-09-16 18:54:01 +0200

    Account for varying placeholder text in examples
    Duckduckgo is serving slightly varying placeholder texts as seen in
    Github actions [1]. I'm guessing it's based on request origin, as I
    can't replicate it locally.
    
    [1] https://github.com/badeball/cypress-cucumber-preprocessor/actions/runs/6208455137

--- examples/webpack-esm/cypress/e2e/duckduckgo.mjs
+++ examples/webpack-esm/cypress/e2e/duckduckgo.mjs
@@ -5,11 +5,12 @@ When("I visit duckduckgo.com", () => {
 });
 
 Then("I should see a search bar", () => {
-  cy.get("input[type=text]").should(
-    "have.attr",
-    "placeholder",
-    "Search the web without being tracked"
-  );
+  cy.get("input[type=text]")
+    .should("have.attr", "placeholder")
+    .and(
+      "match",
+      /Search the web without being tracked|Search without being tracked/
+    );
 
   assert.deepEqual({}, {});
 });
